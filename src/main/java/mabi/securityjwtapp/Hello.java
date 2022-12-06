4cee5b5bfa3598366f9119a81d2919b1d4b0b489
Marcin Blicharz <marcin.blicharz@gmail.com>
2022-12-06 11:31:48 +0100

    resolve compilation problem

--- src/main/java/mabi/securityjwtapp/Hello.java
+++ src/main/java/mabi/securityjwtapp/Hello.java
@@ -1,9 +1,14 @@
 package mabi.securityjwtapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Hello {
-	
+
+	@RequestMapping({"/hello"})
 	public String hello() {
-		return "Hello World!";
+		return "Hello World";
 	}
 
 }
