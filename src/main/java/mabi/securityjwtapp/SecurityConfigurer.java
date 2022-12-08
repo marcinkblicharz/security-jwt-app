0a0768d9767e5d25b83f104ae5b73ae4e455dd98
Marcin Blicharz <marcin.blicharz@gmail.com>
2022-12-08 12:20:29 +0100

    next step for rest authentication method

--- src/main/java/mabi/securityjwtapp/SecurityConfigurer.java
+++ src/main/java/mabi/securityjwtapp/SecurityConfigurer.java
@@ -2,7 +2,9 @@ package mabi.securityjwtapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +22,17 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(myUserDetailsServices);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
 	}
 
 	@Bean
--- src/main/java/mabi/securityjwtapp/models/AuthenticationRequest.java
+++ src/main/java/mabi/securityjwtapp/models/AuthenticationRequest.java
@@ -4,16 +4,38 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class AuthenticationRequest {
 
 	private String username;
 	private String password;
-	
-//	public AuthenticationRequest(String username, String password) {
-//		this.username = username;
-//		this.password = password;
-//	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	//need default constructor for JSON Parsing
+	public AuthenticationRequest()
+	{
+
+	}
+
+	public AuthenticationRequest(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
 }
